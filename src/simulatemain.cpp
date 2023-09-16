6f92a5dfd29d8ea6eb0d275f01ac32a88a487409
Anna Hart <anna19@ada.cs.illinois.edu>
2023-09-16 15:00:21 -0500

    Fixing -r bug and adding error handling

--- src/simulatemain.cpp
+++ src/simulatemain.cpp
@@ -27,6 +27,7 @@ int main(int argc, char** argv)
   int num_cells = 1000;
   double read_depth = .05;
   double alpha_fp = .001;
+  bool _sc = false;
   std::string out_dir = "results";
   std::string dotFilename;
   std::string inputStateTreeFilename;
@@ -46,7 +47,7 @@ int main(int argc, char** argv)
     .refOption("k", "Number of segments (default: 10)", k, false)
     .refOption("l", "Number of mutation clusters (default: 5)", l, false)
     .refOption("e", "The error rate for CNA data (default: 0)", cna_error, false)
-    .refOption("STree", "Output filename for tree (default: none)", outputTreeFilename, false)
+    .refOption("STree", "Output fiSlename for tree (default: none)", outputTreeFilename, false)
     .refOption("SNode", "Output filename for csv with information about nodes, segments, and mutaitons (default: none)", outputNodeFilename, false)
     .refOption("SProportions", "Output filename for csv with information about nodes, segments, and mutaitons (default: none)", outputProportionFilename, false)
     .refOption("dot", "Graphviz DOT output filename (default: '', no output)", dotFilename, false)
@@ -54,7 +55,8 @@ int main(int argc, char** argv)
     .refOption("num_cells", "The number of cells to simulate with the single cell generation (default: 1000)", num_cells, false)
     .refOption("read_depth", "The read_depth for the single cell generation (default: .05)", read_depth, false)
     .refOption("alpha_fp", "The sequencing error for single cell generation (default .001)", alpha_fp, false)
-    .refOption("out_dir", "The output directory for single cell generation (default: results)", out_dir, false);
+    .refOption("out_dir", "The output directory for single cell generation (default: results)", out_dir, false)
+    .refOption("sc", "Generate single cell data", _sc, false);
 
 
   ap.parse();
@@ -125,38 +127,44 @@ int main(int argc, char** argv)
   if (removeUnsampledNodes) 
   {
     Phylogeny newPhylo = phylo.removeUnsampledNodes();
-    //newPhylo.writeDOT(std::cout); //this line
+    newPhylo.writeDOT(std::cout); 
     newPhylo.writeTree(std::cout, outputTreeFilename);
     newPhylo.writeNodeFile(std::cout, outputNodeFilename);
     newPhylo.writeProportionFile(std::cout, outputProportionFilename, m);
-/*     std::cout << newPhylo; 
+
     if (!dotFilename.empty())
     {
       std::ofstream outDOT(dotFilename);
       newPhylo.writeDOT(outDOT);
       outDOT.close();
-    } */
+    } 
   }
-/*   else
+   else
   {
-    std::cout << phylo;
+    phylo.writeDOT(std::cout); 
+    phylo.writeTree(std::cout, outputTreeFilename);
+    phylo.writeNodeFile(std::cout, outputNodeFilename);
+    phylo.writeProportionFile(std::cout, outputProportionFilename, m);
+
     if (!dotFilename.empty())
     {
-      std::ofstream outDOT(dotFilename);
-      phylo.writeDOT(outDOT);
-      outDOT.close();
+    std::ofstream outDOT(dotFilename);
+    phylo.writeDOT(outDOT);
+    outDOT.close();
     }
-  } */
+  }
+  
   
 
-  
-  for (int i = 0; i < m; i++) {
-    SingleCell sc(num_cells, read_depth, alpha_fp, out_dir, k, m, g_rng, cna_error); 
-    sc.loadData(std::cout, outputProportionFilename, outputNodeFilename);
-    sc.generateECDF(std::cout, i);
-    sc.initializeSCS(std::cout); 
-    sc.generateCells(std::cout, i);
-    sc.printSCS(std::cout, i);
+  if (_sc == true) {
+    for (int i = 0; i < m; i++) {
+      SingleCell sc(num_cells, read_depth, alpha_fp, out_dir, k, m, g_rng, cna_error); 
+      sc.loadData(std::cout, outputProportionFilename, outputNodeFilename);
+      sc.generateECDF(std::cout, i);
+      sc.initializeSCS(std::cout); 
+      sc.generateCells(std::cout, i);
+      sc.printSCS(std::cout, i);
+    }
   }
 
   return 0;
